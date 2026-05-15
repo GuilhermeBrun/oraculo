e75889eaf8b13146e2a011dcb0b69101a1e66609
Guilherme Brun Moraes <guilherme.brun@basisti.com.br>
2026-05-14 23:24:55 -0400

    chore: simplifica esqueleto inicial do Electron

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const path = require("node:path");
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+    const window = new BrowserWindow({
+        width: 900,
+        height: 640,
+    });
+
+    window.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
--- src/renderer/renderer.js
+++
@@ -1,9 +0,0 @@
-async function showAppInfo() {
-    const info = await window.oraculo.getAppInfo();
-    const versions = window.oraculo.versions;
-
-    document.querySelector("#app-info").textContent =
-        `${info.name} ${info.version} | Electron ${versions.electron} | Chrome ${versions.chrome} | Node ${versions.node}`;
-}
-
-showAppInfo();
